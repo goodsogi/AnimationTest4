72e3b8e994ff1d5b63efa951472faf1c8ed43a83
Jeonggyu <goodsogi@gmail.com>
2016-08-04 10:48:14 +0900

    first commit

---
+++ app/src/main/java/com/seriouscompany/animationtest4/MainActivity.java
@@ -0,0 +1,32 @@
+package com.seriouscompany.animationtest4;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView sharedImage = (ImageView) findViewById(R.id.sharedimage);
+        sharedImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //This is where the magic happens.
+                // makeSceneTransitionAnimation takes a context, view,
+                // a name for the target view.
+                ActivityOptions options =
+                        ActivityOptions.
+                                makeSceneTransitionAnimation(MainActivity.this, sharedImage, "sharedImage");
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent, options.toBundle());
+            }
+        });
+    }
+}
